91b4e6688988792062bc645238bb6a88e11dcbe1
Oishik Mukhopadhyay <oishik.mukhopadhyay@gmail.com>
2019-04-23 02:16:58 +0530

    Add files via upload

---
+++ Version1.0/display_all.sh
@@ -0,0 +1,17 @@
+clear
+
+echo -e "\n--DATABASES--"
+
+echo -e "\nNAME :\n"
+cat databases//name.csv
+
+echo -e "\nADDRESS :\n"
+cat databases//address.csv
+
+echo -e "\nEMAIL :\n"
+cat databases//email.csv
+
+echo -e "\nRELATIVE :\n"
+cat databases//relative.csv
+
+echo -e "\n"
